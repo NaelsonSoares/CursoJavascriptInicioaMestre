5565dd6fe488e32237044c814f2c4505c3f55b87
Naelson Soares <72839628+NaelsonSoares@users.noreply.github.com>
2024-01-04 22:23:32 -0300

    Aulas javascript
    Javascript até expert

--- AulasJavaScript/javascript/script.js
+++ AulasJavaScript/javascript/script.js
@@ -7,39 +7,39 @@ function atualizarNome() {
   let nome = prompt();
   para.textContent = `Bem vindo ao curso de Javascript! ${nome}`;
 }
-// CONCATENAÇÃO SIMPLES COM O SINAL DE " + "
-let nome1 = document.querySelector('.nome2');
-nome1.addEventListener('click', digiteNome);
-function digiteNome() {
-  let nome2 = prompt();
-  nome1.textContent = nome2;
+  // CONCATENAÇÃO SIMPLES COM O SINAL DE " + "
+  let nome1 = document.querySelector('.nome2');
+  nome1.addEventListener('click', digiteNome);
+    function digiteNome() {
+      let nome2 = prompt();
+      nome1.textContent = nome2;
 
     let idade1 = document.querySelector('.idade2');
     idade1.addEventListener('click', digiteIdade);
-    function digiteIdade() {
-      let idade2 = prompt();
-      idade1.textContent = idade2;
+      function digiteIdade() {
+        let idade2 = prompt();
+          idade1.textContent = idade2;
 
       let frase1 = document.querySelector('.frase2');
       frase1.addEventListener('click', concatenar);
-      function concatenar() {
-        let frase2 = 'Meu nome é ' + nome2 +' e tenho ' + idade2 + ' anos!';
-        frase1.textContent = ' '+ frase2;
-
-        // CONCATENAÇÃO COM O "CIFRÃO ( $ )" SINTAXE RECOMENDADA
-        let frase3 = document.querySelector('.frase4');
-        frase3.addEventListener('click', concatenar);
         function concatenar() {
-          let frase4 = `Meu nome é  ${nome2}  e tenho ${idade2} anos!`;
-          frase3.textContent = ' '+ frase4;
+         let frase2 = 'Meu nome é ' + nome2 +' e tenho ' + idade2 + ' anos!';
+           frase1.textContent = ' '+ frase2;
 
-        // TIPOS DE VARIÁVEIS: STRING, NUMBER
-        let frase5 = document.querySelector('.tipos_var');
-        frase5.addEventListener('click', tipos);
-        function tipos() {
-          let tipoString = nome2;
-          let tipoNumber = parseInt(idade2);  // CONVERSÃO DE TIPOS DE VARIÁVEIS DE STRING PARA NUMBER (parseInt e parseFloat e toString)
-          frase5.textContent = `${nome2} é uma ${typeof tipoString} e ${idade2} é um ${typeof tipoNumber}` ;
+ // CONCATENAÇÃO COM O "CIFRÃO ( $ )" SINTAXE RECOMENDADA
+let frase3 = document.querySelector('.frase4');
+ frase3.addEventListener('click', concatenar);
+  function concatenar() {
+   let frase4 = `Meu nome é  ${nome2}  e tenho ${idade2} anos!`;
+     frase3.textContent = ' '+ frase4;
+
+ // TIPOS DE VARIÁVEIS: STRING, NUMBER
+let frase5 = document.querySelector('.tipos_var');
+frase5.addEventListener('click', tipos);
+  function tipos() {
+    let tipoString = nome2;
+      let tipoNumber = parseInt(idade2);  // CONVERSÃO DE TIPOS DE VARIÁVEIS DE STRING PARA NUMBER (parseInparseFloat e toString)
+      frase5.textContent = `${nome2} é uma ${typeof tipoString} e ${idade2} é um ${typeof tipoNumber}` ;
         }
       }
     }
@@ -69,9 +69,9 @@ function entradaqui() {
   // OPERADORES ARITMÉTICOS ( +, -, *, /, %, ** )
   let num1 = document.querySelector('.digita1');
   num1.addEventListener('click', primeironum);
-  function primeironum() {
-  let digito1 = prompt();
-  num1.textContent = digito1;
+    function primeironum() {
+      let digito1 = prompt();
+      num1.textContent = digito1;
 
   let num2 = document.querySelector('.digita2');
   num2.addEventListener('click', segundonum);
@@ -361,6 +361,7 @@ function entradaqui() {
           
             let calculo = document.querySelector('.calcular');
             calculo.addEventListener('click', operacao);
+           // const operacao = () => { // ARROW FUNCTION
             function operacao() {
                 let calcular = parseInt( prompt( ) )
                 
@@ -438,3 +439,12 @@ function entradaqui() {
             }while(loopdowhile <= 10)
           }
       } 
+
+// ARROW FUNCTION RECEBENDO PARÂMETROS
+let entrada13 = document.querySelector ('.arrow_funcao');
+entrada13.addEventListener('click', arrow_f);
+const arrow_f = ( n1 = 0, n2 = 0 ) => {
+  n1 = parseInt( prompt( ) );
+  n2 = parseInt( prompt( ) );
+}
+entrada13.textContent = n1 * n2;
